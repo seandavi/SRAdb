3357184d77cdef8ec1692eede49e2fe83b4346f1
Jack Zhu <zhujack@mail.nih.gov>
2010-04-16 01:33:25 +0000

    getSRAdbFile - download sqlite database from: http://gbnci.abcc.ncifcrf.gov/backup/SRAmetadb.sqlite.gz
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SRAdb@46085 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getSRAdbFile.R
+++ R/getSRAdbFile.R
@@ -6,8 +6,10 @@ getSRAdbFile <-
         method <- ifelse(!is.null(getOption("download.file.method")),
                          getOption("download.file.method"), "auto")
     localfile <- file.path(destdir, destfile)
-    download.file("http://watson.nci.nih.gov/~zhujack/SRAmetadb.sqlite.gz", 
-                  destfile = localfile, mode = "wb", method=method)
+    # download.file("http://watson.nci.nih.gov/~zhujack/SRAmetadb.sqlite.gz", 
+    #              destfile = localfile, mode = "wb", method=method)
+	download.file("http://gbnci.abcc.ncifcrf.gov/backup/SRAmetadb.sqlite.gz", 
+				  destfile = localfile, mode = "wb", method=method)	
     message("Unzipping...\n")
     gunzip(localfile, overwrite = TRUE)
     unzippedlocalfile <- gsub("[.]gz$", "", localfile)
