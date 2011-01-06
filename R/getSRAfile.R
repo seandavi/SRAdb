2ed60577fcc7992b5e470bc80ca9f574bcb8b635
Jack Zhu <zhujack@mail.nih.gov>
2011-01-06 21:14:26 +0000

    Added functions for manipulating sra or sra-lite files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SRAdb@51790 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/getSRAfile.R
@@ -0,0 +1,23 @@
+getSRAfile <-
+function (in_acc, sra_con, destdir, sraType='litesra', makeDirectory = FALSE, method) 
+{
+    if (missing(method))
+        method <- ifelse(!is.null(getOption("download.file.method")),
+                         getOption("download.file.method"), "auto")
+	sraFileInfo = getSRAinfo( in_acc, sra_con=sra_con, sraType )  
+    if ( makeDirectory==TRUE && !file.exists(destdir) ) {
+        tryCatch(dir.create( destdir ),
+                 error=function(err) {
+                     stop("failed to create '", destdir, "': ",
+                          conditionMessage(err))
+                 })
+    }
+ 	message( "Files are saved to: \n'", destdir, "'\n" )
+    fnames <- sraFileInfo$sra
+    fileinfo = NULL
+    for (i in fnames) {
+        download.file(i, destfile = file.path(destdir, basename(i)),
+                      method=method)
+    }
+}
+
