bb8175abd3f48cb59e28d5a6c393f5c4aa20a17c
Jack Zhu <zhujack@mail.nih.gov>
2011-12-01 03:14:49 +0000

    Added Dropbox URL as an alternative downloading source
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SRAdb@61005 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getSRAdbFile.R
+++ R/getSRAdbFile.R
@@ -5,15 +5,22 @@ getSRAdbFile <-
     if (missing(method))
         method <- ifelse(!is.null(getOption("download.file.method")),
                          getOption("download.file.method"), "auto")
-    localfile <- file.path(destdir, destfile)
-    
+    localfile <- file.path(destdir, destfile)   
     options(warn=-1)
-    url_sra = "http://watson.nci.nih.gov/~zhujack/SRAmetadb.sqlite.gz"
-    con.url_sra <- try(url(url_sra, open='rb'), silent = TRUE)
-    if(inherits(con.url_sra, "try-error"))
-    	url_sra = "http://gbnci.abcc.ncifcrf.gov/backup/SRAmetadb.sqlite.gz"
+    
+    url_sra_1 = 'http://gbnci.abcc.ncifcrf.gov/backup/SRAmetadb.sqlite.gz'
+    url_sra_2 = 'http://watson.nci.nih.gov/~zhujack/SRAmetadb.sqlite.gz'
+    url_sra_3 = 'http://dl.dropbox.com/u/51653511/SRAmetadb.sqlite.gz'
+
+    if(! inherits(try(url(url_sra_1, open='rb'), silent = TRUE), "try-error") ) {
+       url_sra = url_sra_1
+    } else if(! inherits(try(url(url_sra_2, open='rb'), silent = TRUE), "try-error") ) {
+       url_sra = url_sra_2
+    } else {
+       url_sra = url_sra_3
+    }
+
     download.file(url_sra, destfile = localfile, mode = "wb", method=method)
-	
     message("Unzipping...\n")
     gunzip(localfile, overwrite = TRUE)
     unzippedlocalfile <- gsub("[.]gz$", "", localfile)
