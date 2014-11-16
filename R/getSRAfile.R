9391afc5c68f35f3e723ba6ac8217686c80a2fbc
Jack Zhu <zhujack@mail.nih.gov>
2014-11-16 17:28:08 +0000

    Due to the fact that EBI/ENA removed 'report' web service, getFASTQinfo.R did no work any more, in this commit:
     1. A new table, 'fastq',  has been added  to the SRAmetadb.sqlite.gz
      2.  The getFASTQinfo.R function has been rewritten and the fast table is used for getting fastq information.
    
    Also because SRA/NCBI phased out the listsra data files, the option for 'litesra' has been removed in all functions in this commit.
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SRAdb@96799 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getSRAfile.R
+++ R/getSRAfile.R
@@ -1,11 +1,12 @@
-## getSRAfile is to download .sra or .lite.sra from NCBI or fastq files from EBI ENA ftp site for a given list of SRA accessions
+## getSRAfile is to download .sra from NCBI or fastq files from EBI ENA ftp site for a given list of SRA accessions
 ## ascpCMD = 'ascp -QT -l 300m -i /usr/local/aspera/connect/etc/asperaweb_id_dsa.putty'
 
-## Example: sraFiles = getSRAfile (c("SRR000648","SRR000657"), sra_con, destDir=getwd(), fileType='litesra', srcType='fasp', makeDirectory=FALSE, method='curl', ascpCMD=ascpCMD) 
+## Example: sraFiles = getSRAfile (c("SRR000648","SRR000657"), sra_con, destDir=getwd(), fileType='sra', srcType='fasp', makeDirectory=FALSE, method='curl', ascpCMD=ascpCMD) 
 
 getSRAfile <-
-function (in_acc, sra_con, destDir=getwd(), fileType='litesra', srcType='ftp', makeDirectory=FALSE, method='curl', ascpCMD=NULL) 
+function (in_acc, sra_con, destDir=getwd(), fileType='sra', srcType='ftp', makeDirectory=FALSE, method='curl', ascpCMD=NULL) 
 {
+	## note: 'litesra' has phased out 
 	sraFiles = listSRAfile (in_acc, sra_con, fileType, srcType)
 	
 	if ( makeDirectory==TRUE && !file.exists(destDir) ) {
