68d036ece84f11c387a7fc747e76fcd8f6e55e4f
Jack Zhu <zhujack@mail.nih.gov>
2017-03-08 20:23:27 +0000

    Fixed getFASTQinfor URL according to http://www.ebi.ac.uk/ena/browse/read-download#archive_generated_fastq_files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SRAdb@127256 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getFASTQinfo.R
+++ R/getFASTQinfo.R
@@ -22,28 +22,34 @@ function (in_acc, sra_con, srcType = 'ftp') {
 	sra_acc_run = paste("'", paste(sra_acc$run, collapse = "','"),"'", sep="");
 	sql <- paste ("SELECT * FROM fastq WHERE run_accession IN (", sra_acc_run, ")", sep = "");				  			 
 	rs_fq <- dbGetQuery(sra_con, sql);	
-	names(rs_fq) <- sub('fastq_accession', 'run', names(rs_fq))
-	names(rs_fq) <- sub('run_accession', 'run', names(rs_fq))
-	names(rs_fq) <- sub('file_name', 'ftp', names(rs_fq))	
-	
-	## format ftp URLs
-	## Note EBI added <dir2> for run accesions wirh over 6 digits -details please see http://www.ebi.ac.uk/ena/browse/read-download#archive_generated_fastq_files
-	
-	for( i in 1:length(rs_fq$run) ) {
-		run1 = rs_fq$run[i]
-		if( nchar(run1) < 10 ) {
-			rs_fq$ftp[i] <- file.path('ftp://ftp.sra.ebi.ac.uk/vol1/fastq', substring(run1, 1, 6), run1, rs_fq$ftp[i])
-		} else {
-			dir2 <- paste( c(rep(x='0', 12-nchar(run1)), substring(run1, 10, nchar(run1)) ), collapse = '' )
-			rs_fq$ftp[i] <- file.path('ftp://ftp.sra.ebi.ac.uk/vol1/fastq', substring(run1, 1, 6), dir2, run1, rs_fq$ftp[i])
-		}	
-	}
-	## merge acc to the fastq ftp
-	fastqFiles <- merge(sra_acc, rs_fq, by='run', all.x=TRUE) 
-	if( srcType == 'fasp' ) {
-		fastqFiles$ftp <- sub('ftp://ftp.sra.ebi.ac.uk/', 'era-fasp@fasp.sra.ebi.ac.uk:', fastqFiles$ftp)	
-		names(fastqFiles) <- sub('ftp', 'fasp', names(fastqFiles))	
-	} 
-	return(fastqFiles);
+    names(rs_fq) <- sub("run_accession", "run", names(rs_fq))
+    
+    df_ftp = NULL
+    for (i in 1:length(rs_fq$run)) {
+        run1 = rs_fq$run[i]
+        if (nchar(run1) < 10) {
+            df_ftp_1 <- as.data.frame( cbind("run" = run1, "ftp" = file.path("ftp://ftp.sra.ebi.ac.uk/vol1/fastq",                                       substring(run1, 1, 6), run1, paste(run1, ".fastq.gz", sep="") )))
+        } else {
+            ## format ftp URLs
+            ## Note EBI added <dir2> for run accesions with over 6 digits - details please see http://www.ebi.ac.uk/ena/browse/read-download#archive_generated_fastq_files
+            dir2 <- paste("00", substring(run1, nchar(run1), nchar(run1)), sep="")
+            if( rs_fq$FASTQ_FILES[i] > 1 ) {
+                fastqNames = paste(run1, "_", 1:rs_fq$FASTQ_FILES[i], ".fastq.gz", sep="")
+            } else {
+                fastqNames = paste(run1, ".fastq.gz", sep="")
+            }
+            df_ftp_1 <- as.data.frame( cbind("run" = run1, "ftp" = file.path("ftp://ftp.sra.ebi.ac.uk/vol1/fastq",                                       substring(run1, 1, 6), dir2, run1, fastqNames)))
+        }
+        
+        df_ftp = rbind(df_ftp, df_ftp_1)
+    }
+    fastqFiles <- merge(sra_acc, df_ftp, by = "run")
+    if (srcType == "fasp") {
+        fastqFiles$ftp <- sub("ftp://ftp.sra.ebi.ac.uk/", "era-fasp@fasp.sra.ebi.ac.uk:", 
+                              fastqFiles$ftp)
+        names(fastqFiles) <- sub("ftp", "fasp", names(fastqFiles))
+    }
+    return(fastqFiles)
 }
+
 
